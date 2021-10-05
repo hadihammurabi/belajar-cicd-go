8c711beabe9a1aa2490b9dbf43c4926913632492
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-05 16:49:14 +0700

    update response

--- route/route.go
+++ route/route.go
@@ -6,10 +6,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type response struct {
+	Message string `json:"message"`
+	Data    string `json:"data"`
+}
+
 func NewRoute(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, echo.Map{
-			"data": "halo semua!",
+		return c.JSON(http.StatusOK, response{
+			Message: "success",
+			Data:    "halo epribadeh!",
 		})
 	})
 }
