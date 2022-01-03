3c0569f1e5048e82127ed58e7a90f9c467c68c4f
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-01-03 20:51:26 +0700

    change main

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-// 	"simple-go/config"
-// 	"simple-go/route"
+	// 	"simple-go/config"
+	// 	"simple-go/route"
+
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-// 	config.InitDB()
+	// 	config.InitDB()
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, response{
-			Message: "success",
-			Data:    "halo epriwan!",
+		return c.JSON(http.StatusOK, echo.Map{
+			"Message": "success",
+			"Data":    "halo epriwan!",
 		})
 	})
 
-// 	route.NewRoute(e)
+	// 	route.NewRoute(e)
 	panic(e.Start(":8080"))
 }
